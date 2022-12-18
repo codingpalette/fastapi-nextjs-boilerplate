77933b2e8949677f392dec85938e01d3123ada6c
codingpalette <codingpalette@gmail.com>
2022-12-18 14:50:05 +0900

    환경변수 테스트

--- back/api/v1/endpoints/users.py
+++ back/api/v1/endpoints/users.py
@@ -5,7 +5,7 @@ router = APIRouter()
 
 @router.get('/test')
 async def user_test():
-    name = os.getenv("MY_NAME")
+    name = os.getenv("MY_NAME", "홍길동")
     print(f"Hello {name} from Python")
     print('user_test')
     return {"data": name}
