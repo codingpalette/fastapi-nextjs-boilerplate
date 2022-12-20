e25350f8ef4dd4863c50230acbfdeceffd9339ac
codingpalette <codingpalette@gmail.com>
2022-12-20 14:45:13 +0900

    theme 지정

--- front/app/template.tsx
+++ front/app/template.tsx
@@ -4,6 +4,8 @@ import {useQuery, useQueryClient} from "@tanstack/react-query";
 import fetcher from "../lib/fetcher";
 import {useEffect} from "react";
 import Header from "../components/base/Header";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
 const template = ({children}: { children: React.ReactNode }) => {
   const {data: me, isLoading, isError, error} = useQuery({
@@ -15,15 +17,28 @@ const template = ({children}: { children: React.ReactNode }) => {
     console.log('me', me)
   }, [me])
 
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'light',
+    },
+  });
+
+  // https://github.com/codingpalette/next-fastapi-blog/blob/main/front3/components/Layout/index.js 참고
+
   return(
     <>
-      {me && (
-        <>
-          <Header />
-        </>
-      )}
-      <h1>templage</h1>
-      {children}
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        {me && (
+          <>
+            <Header />
+          </>
+        )}
+        <h1>templage</h1>
+        {children}
+      </ThemeProvider>
+
+
     </>
   )
 }
