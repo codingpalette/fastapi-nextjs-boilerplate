c73c429220b196e3f706d8944262c32aa386a979
codingpalette <codingpalette@gmail.com>
2023-01-14 12:44:54 +0900

    emotion cache 적용

---
+++ front2/components/wrapper/EmotionWrapper.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+import { useServerInsertedHTML } from 'next/navigation';
+import { useState } from 'react';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const EmotionWrapper = ({children}: Props) => {
+  const [cache] = useState(() => {
+    const cache = createCache({ key: "css" });
+    cache.compat = true;
+    return cache;
+  });
+
+  useServerInsertedHTML(() => {
+    return (
+      <style
+        data-emotion={`${cache.key} ${Object.keys(cache.inserted).join(" ")}`}
+        dangerouslySetInnerHTML={{
+          __html: Object.values(cache.inserted).join(" "),
+        }}
+      />
+    );
+  });
+
+  return <CacheProvider value={cache}>{children}</CacheProvider>;
+}
+
+export default EmotionWrapper
