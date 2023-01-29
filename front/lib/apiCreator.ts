e09ce2fd8f655c9c52bad1301b203a136b236819
codingpalette <codingpalette@gmail.com>
2023-01-29 15:03:29 +0900

    front 삭제

--- front/lib/apiCreator.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-import {backUrl} from "../config/config";
-
-axios.defaults.baseURL = backUrl
-axios.defaults.timeout = 180000;
-axios.defaults.withCredentials = true;
-
-export default axios;
--- front/lib/fetcher.ts
+++
@@ -1,5 +0,0 @@
-import apiCreator from "./apiCreator";
-
-const fetcher = (url: string) => apiCreator.get(url, { withCredentials: true }).then((response) => response.data);
-
-export default fetcher;
